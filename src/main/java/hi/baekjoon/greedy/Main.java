e63b5e5ef19e6640a2ae37daf43dc9f09bceb78c
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-02-08 20:23:21 +0900

    거스름돈

---
+++ src/main/java/hi/baekjoon/greedy/Main.java
@@ -0,0 +1,62 @@
+package hi.baekjoon.greedy;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String s = br.readLine();
+        int money = Integer.parseInt(s);
+        int result = solution(1000 - money);
+        bw.write(String.valueOf(result));
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static int solution(int num) {
+        int a =0; //500
+        int b =0; //100
+        int c =0; //50
+        int d =0; //10
+        int e =0; //5
+        int f =0; //1
+
+        while (true) {
+            if(num >= 500){
+                a++;
+                num-=500;
+            }else if(num >= 100){
+                b++;
+                num -=100;
+            }else if(num >= 100){
+                b++;
+                num -=100;
+            }else if(num >= 50){
+                c++;
+                num -=50;
+            }else if(num >= 10){
+                d++;
+                num -=10;
+            }else if(num >= 5){
+                e++;
+                num -=5;
+            }else if(num >= 1){
+                b++;
+                num -=1;
+            }
+            else{
+                break;
+            }
+        }
+        return a+b+c+d+e+f;
+    }
+
+}
+
