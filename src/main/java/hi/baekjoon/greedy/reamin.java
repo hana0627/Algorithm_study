199c071916fd64a2b24b4fc939473c846d55ad56
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-02-11 21:02:58 +0900

    반례찾기 위해서 10문제맞추기...

---
+++ src/main/java/hi/baekjoon/greedy/reamin.java
@@ -0,0 +1,67 @@
+package hi.baekjoon.greedy;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class reamin {
+    //https://www.acmicpc.net/problem/11047
+    // TODO 반례못찾겠음
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String param1 = br.readLine();
+        String[] s = param1.split(" ");
+        List<Integer> list = new ArrayList<>();
+        int ints[] = new int[Integer.parseInt(s[0])];
+        for(int i = 0; i<Integer.parseInt(s[0]); i++) {
+            ints[i] = Integer.parseInt(br.readLine());
+        }
+
+        int result = solution(Integer.parseInt(s[1]), ints);
+        bw.write(result);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    private static int solution(int total,int[] list) {
+
+        int tmp = 0;
+        for(int i =0 ; i < list.length; i++) {
+            for(int t = i+1; t<list.length; t++ ) {
+                if(list[i] < list[t]){
+                    tmp = list[i];
+                    list[i] = list[t];
+                    list[t] = tmp;
+                }
+            }
+        }
+
+        System.out.println("여기확인");
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+
+        int result = 0;
+
+        tmp = total;
+        while(true) {
+            System.out.println("빙글빙글");
+            for(int i =0; i < list.length; i++) {
+                if(tmp>=list[i]) {
+                    tmp -= list[i];
+                    result++;
+                    System.out.println("tmp -> " + tmp);
+                    System.out.println("result -> " + result);
+                    break;
+                }
+                if(tmp == 0) {
+                    return result;
+                }
+            }
+        }
+
+    }
+}
