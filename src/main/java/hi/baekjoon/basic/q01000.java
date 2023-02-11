40a074b191a99e4622ed45d60e87338ac72cc1e3
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-02-11 21:06:44 +0900

    10문제맞추기...

---
+++ src/main/java/hi/baekjoon/basic/q01000.java
@@ -0,0 +1,17 @@
+package hi.baekjoon.basic;
+
+import java.util.Scanner;
+
+public class q01000 {
+    public static void main(String[] args) {
+
+        int num1,num2;
+
+        Scanner s = new Scanner(System.in);
+        num1 = s.nextInt();
+        num2 = s.nextInt();
+
+        System.out.println(num1+num2);
+        s.close();
+    }
+}
