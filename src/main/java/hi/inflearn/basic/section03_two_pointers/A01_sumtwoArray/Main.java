abd1c99efce6dad4233fff1eb1fdbf555fdeb6c0
hana0627 <hanana9506@naver.com>
2023-08-18 18:10:55 +0900

    두배열합치기

--- src/main/java/hi/inflearn/basic/section03_two_pointers/A01_sumtwoArray/Main.java
+++ src/main/java/hi/inflearn/basic/section03_two_pointers/A01_sumtwoArray/Main.java
@@ -1,8 +1,8 @@
 package hi.inflearn.basic.section03_two_pointers.A01_sumtwoArray;
 
 import java.util.Scanner;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
     /*
@@ -14,11 +14,11 @@ public class Main {
     입력
     첫 번째 줄에 첫 번째 배열의 크기 N(1<=N<=100)이 주어집니다.
 
-    두 번째 줄에 N개의 배열 원소가 오름차순으로 주어집니다.
+    두 번째 줄에 N개의 배열 원소가 **오름차순으로** 주어집니다.
 
     세 번째 줄에 두 번째 배열의 크기 M(1<=M<=100)이 주어집니다.
 
-    네 번째 줄에 M개의 배열 원소가 오름차순으로 주어집니다.
+    네 번째 줄에 M개의 배열 원소가 **오름차순으로** 주어집니다.
 
     각 리스트의 원소는 int형 변수의 크기를 넘지 않습니다.
 
@@ -40,21 +40,34 @@ public class Main {
 
 
     public void solution (int n, int m, int[] arr1, int[] arr2) {
+        int p1 = 0;
+        int p2 = 0;
+        int len = n+m;
 
-        //너무 양아치처럼 풀었나... 맞잖아 이거..
+        List<Integer> answer = new ArrayList<>();
 
-        int answer = 0;
 
-        Set<Integer> set = new HashSet<>();
+        for(int i =0; i<len; i++) {
+            // if문 두개 대신에 while조건의 풀이도 있음
+            if(p1 == n) {
+                answer.add(arr2[p2++]);
+            }
 
-        for(int i : arr1) {
-            set.add(i);
+            else if(p2 == m) {
+                answer.add(arr1[p1++]);
+            }
+
+
+            else if(arr1[p1] >= arr2[p2]) {
+                answer.add(arr2[p2++]);
+            }
+            else if(arr2[p2] > arr1[p1]) {
+                answer.add(arr1[p1++]);
+            }
         }
-        for(int i : arr2) {
-            set.add(i);
-        }
 
-        set.stream().sorted().forEach(i -> {
+
+        answer.forEach(i -> {
             System.out.print(i + " ");
         });
 
@@ -83,3 +96,81 @@ public class Main {
         sc.close();
     }
 }
+
+
+/*
+
+        // 시간복잡도 생각 O(NlogN) -> O(N)
+         int len = n+m;
+
+        int[] answer = new int[len];
+
+        for(int i = 0; i<n ; i++) {
+            answer[i] = arr1[i];
+        }
+        for(int i = 0; i<m ; i++) {
+            answer[i+n] = arr2[i];
+        }
+
+        int tmp;
+        for(int i =0; i<len; i++) {
+            for(int k = i ; k<len; k++) {
+                if(answer[i] > answer[k]) {
+                    tmp = answer[i];
+                    answer[i] = answer[k];
+                    answer[k] = tmp;
+
+                }
+            }
+        }
+
+        for(int i : answer) {
+            System.out.print(i + " ");
+        }
+
+ */
+
+
+
+
+/*
+
+        강의에서 이렇게 풀지말라함...
+
+        int[] answer = new int[n+m];
+
+        for(int i = 0; i<n ; i++) {
+            answer[i] = arr1[i];
+        }
+        for(int i = 0; i<m ; i++) {
+            answer[i+n] = arr2[i];
+        }
+
+        Arrays.sort(answer);
+        for(int x : answer) {
+            System.out.print(x + " ");
+        }
+
+ */
+
+/*
+   //너무 양아치처럼 풀었나... 맞잖아 이거.. 
+   
+    // 문제를 잘못읽었음. 중복제거 안해도 됨, 훨씬쉬움
+
+        int answer = 0;
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : arr1) {
+            set.add(i);
+        }
+        for(int i : arr2) {
+            set.add(i);
+        }
+
+        set.stream().sorted().forEach(i -> {
+            System.out.print(i + " ");
+        });
+
+ */
